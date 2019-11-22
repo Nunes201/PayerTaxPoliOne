e5e4962a979a08d1327f761511c87a7155844a21
Nunes201 <nunesjhe@hotmail.com>
2019-11-22 09:30:46 -0300

    Primeiro projeto usando polimorfismo

---
+++ src/entities/Company.java
@@ -0,0 +1,31 @@
+package entities;
+
+public class Company extends TaxPayer {
+
+	private int numberOfEmployees;
+
+	public Company(String name, double anualIncome, int numberOfEmployees) {
+		super(name, anualIncome);
+		this.numberOfEmployees = numberOfEmployees;
+	}
+
+	public int getNumberOfEmployees() {
+		return numberOfEmployees;
+	}
+
+	public void setNumberOfEmployees(int numberOfEmployees) {
+		this.numberOfEmployees = numberOfEmployees;
+	}
+
+	@Override
+	public double tax() {
+		double sum = 0.0;
+		if (numberOfEmployees <= 10) {
+			sum = getAnualIncome() * 0.16;
+		} else {
+			sum = getAnualIncome() * 0.14;
+		}
+		return sum;
+	}
+
+}
